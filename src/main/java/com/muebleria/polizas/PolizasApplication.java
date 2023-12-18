cfb64bd6e06987419f56dc8895a0f54afb2ee376
hhg.mzt <hiram.hdezg@gmail.com>
2023-12-18 11:42:43 -0700

    chore: se habilita cors

--- src/main/java/com/muebleria/polizas/PolizasApplication.java
+++ src/main/java/com/muebleria/polizas/PolizasApplication.java
@@ -2,6 +2,9 @@ package com.muebleria.polizas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PolizasApplication {
@@ -11,9 +14,21 @@ public class PolizasApplication {
 	}
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(PolizasApplication.class, args);
 		System.out.println("Arrancando API REST Mueblería");
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigure(){
+		String[] allowDomains = new String[1];
+		allowDomains[0] = "http://localhost:4200";
+		System.out.println("CORS configuration...");
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry){
+				registry.addMapping("/**").allowedOrigins(allowDomains);
+			}
+		};
 	}
 
 }
